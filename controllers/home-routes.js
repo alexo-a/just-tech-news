48b65bd40b5dd9eb4b6b4a7899f98c4116bdf077
alexo-a <a.ostrowski.11@gmail.com>
2020-08-29 10:24:06 -0500

    fixed lost css file and made login functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
         attributes: [
             'id',
@@ -36,5 +38,11 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
-
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
 module.exports = router;
